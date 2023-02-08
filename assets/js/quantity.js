31b4bab0eb47851bfbf1ae2314d5eb926f6322e9
kofeinastudio <124392934+kofeinastudio@users.noreply.github.com>
2023-02-08 10:43:22 +0100

    Add files via upload

--- assets/js/quantity.js
+++ assets/js/quantity.js
@@ -1,24 +1,24 @@
 $(document).ready(function(){
     var quantitiy=0;
-   $('.quantity-right-plus').click(function(e){
+    $('.quantity-right-plus').click(function(e){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        var quantity = parseInt($('#quantity').val());
+        var quantity = parseInt($(this).siblings('.text-center').val());
         // If is not undefined
-            $('#quantity').val(quantity + 1);
-            // Increment
+        $(this).siblings('.text-center').val(quantity + 1);
+        // Increment
     });
 
-     $('.quantity-left-minus').click(function(e){
+    $('.quantity-left-minus').click(function(e){
         // Stop acting like a button
         e.preventDefault();
         // Get the field name
-        var quantity = parseInt($('#quantity').val());
+        var quantity = parseInt($(this).siblings('.text-center').val());
         // If is not undefined
-            // Increment
-            if(quantity>1){
-            $('#quantity').val(quantity - 1);
-            }
+        // Increment
+        if(quantity>1){
+            $(this).siblings('.text-center').val(quantity - 1);
+        }
     });
 });
